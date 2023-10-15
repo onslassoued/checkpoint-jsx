fd87a450cb3543f8b10a230db0d203ee63f2a704
ons lassoued <onslassoued2002@gmail.com>
2023-10-15 09:29:58 +0200

    'added'

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Price from './Price';
+import Description from './Description';
+import Product from './Product';
+import Image from './Image';
+import Name from './Name';
+import BasicExample from './BasicExample';
+import Message from './Message';
+
+function App() {
+  return (
+<>
+  <BasicExample></BasicExample>
+  <Message></Message>
+  
+</>
+  );
+}
+
+export default App;
